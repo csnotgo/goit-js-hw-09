cabbf1ef812b861518d1da128be2ab001ac6cbca
csnotgo <109911970+csnotgo@users.noreply.github.com>
2022-10-15 10:59:56 +0000

    Deploying to gh-pages from @ csnotgo/goit-js-hw-09@d10c1a8e02242c327be9d8ececc2565bb01f61c4 🚀

---
+++ 01-color-switcher.33dfeb88.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("body"),t=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]");t.addEventListener("click",(function(){t.disabled=!0,n.disabled=!1,a=setInterval((function(){e.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),n.addEventListener("click",(function(){n.disabled=!0,t.disabled=!1,clearInterval(a)}));var a=null}();
+//# sourceMappingURL=01-color-switcher.33dfeb88.js.map
